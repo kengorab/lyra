68ad4a7ccc320ba7e72ebaa8ee61aa7e73d2de75
Ken Gorab <ken.gorab@gmail.com>
2018-09-10 22:30:39 -0400

    Added functionality for a growable list (not in use yet, but will be in the future)

---
+++ src/common/list.c
@@ -0,0 +1,5 @@
+//
+// Created by Ken Gorab on 9/10/18.
+//
+
+#include "list.h"
---
+++ src/common/list.h
@@ -0,0 +1,8 @@
+//
+// Created by Ken Gorab on 9/10/18.
+//
+
+#ifndef CLYRA_LIST_H
+#define CLYRA_LIST_H
+
+#endif //CLYRA_LIST_H
